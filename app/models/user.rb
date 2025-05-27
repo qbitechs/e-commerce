b213d17f9774bda5fcfc49d945502ada79384348
TayyabAli491 <alihaniff5@gmail.com>
2025-05-27 17:20:29 +0500

    Setup User model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum :role, {
+    customer: "customer",
+    admin: "admin"
+  }, _default: :customer
+end
