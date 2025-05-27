41b6a551e04a47a6bdaf81db3b415e30e4cab2e5
TayyabAli491 <alihaniff5@gmail.com>
2025-05-27 18:35:24 +0500

    Create Product CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
 
   authenticated :user do
     root to: "dashboard#index", as: :authenticated_root
   end
 
+  resources :products
   root to: "static#index"
 end
---
+++ db/migrate/20250527132040_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock
+      t.string :sku
+
+      t.timestamps
+    end
+  end
+end
