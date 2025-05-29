65a97074808865d618fabbed758b57a9121df4b7
TayyabAli491 <alihaniff5@gmail.com>
2025-05-29 18:22:50 +0500

    Create order controller for admin index show

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,27 @@
+class Admin::OrdersController < ApplicationController
+  before_action :set_order, only: %i[show update]
+
+  def index
+    @orders = Order.all(created_at: :desc)
+  end
+
+  def show; end
+
+  def update
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order), notice: "Order status updated."
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+end
