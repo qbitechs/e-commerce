965745e91e18796a2528df6fe69b3b421323cc08
TayyabAli491 <alihaniff5@gmail.com>
2025-05-29 16:12:21 +0500

    Delete unnecessary files

--- app/models/product.rb
+++
@@ -1,5 +0,0 @@
-class Product < ApplicationRecord
-  has_many_attached :images
-  has_many :order_items
-  has_many :orders, through: :order_items
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,2 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :products
-  devise_for :users
-
-  authenticated :user do
-    root to: "dashboard#index", as: :authenticated_root
-  end
-
-  resources :products
-  root to: "static#index"
 end
