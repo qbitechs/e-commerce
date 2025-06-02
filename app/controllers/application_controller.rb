1c0f3fdae38814329f5a7b208685a6424e9e6990
TayyabAli491 <alihaniff5@gmail.com>
2025-06-02 17:39:56 +0500

    set cuurent request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_customer, :current_admin, :current_cart
+
+  private
+
+  def current_customer
+    return unless user_signed_in? && current_user.role == "customer"
+    current_user.customer
+  end
+
+  def current_admin
+    return unless user_signed_in? && current_user.role == "admin"
+    current_user.admin
+  end
+
+  def current_cart
+    current_customer&.current_cart
+  end
 end
