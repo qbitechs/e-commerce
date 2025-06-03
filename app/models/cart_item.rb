03778f22bdc77f4dd645284b7933b9881283b0fe
TayyabAli491 <alihaniff5@gmail.com>
2025-06-03 15:18:27 +0500

    Write model level logic of cart

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,10 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # def item_total
+  #   quantity * product.price
+  # end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
   belongs_to :user
-  has_one    :cart
+  has_one    :cart, dependent: :destroy
   has_many   :orders, dependent: :destroy
+
+  after_create :create_cart
 end
