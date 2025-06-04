b49f54c094769c27cb180f83825ea9cb8fde7a17
TayyabAli491 <alihaniff5@gmail.com>
2025-06-04 21:08:36 +0500

    Add nav classes dynamically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link_classes(path)
+    if current_page?(path)
+      "text-indigo-600 font-medium flex items-center py-1 border-b-2 border-indigo-500"
+    else
+      "text-gray-600 hover:text-indigo-600 transition-colors font-medium flex items-center py-1 border-b-2 border-transparent hover:border-indigo-300"
+    end
+  end
 end
