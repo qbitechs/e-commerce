fcd65ca6dd7519b9882cdef8781b6bc2a7ccd05c
TayyabAli491 <alihaniff5@gmail.com>
2025-06-04 20:00:07 +0500

    Add disable add to cart logic

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -2,10 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  connect() {
-  }
+  increment(event) {
+    event.target.disabled = true
+    event.target.textContent = "Adding..."
+        
+    event.target.classList.add("opacity-50", "cursor-not-allowed")
+    event.target.classList.remove("hover:from-indigo-700", "hover:to-purple-700")
 
-  increment() {
+    this.disableButton(event.currentTarget);
+    
     const countElement = document.getElementById("count");
     const count = Number(countElement.textContent) || 0;
     countElement.textContent = count + 1;
