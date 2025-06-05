d2b7b75389629a5b1cc6cb6b1e0b7ea3c7d9b8a9
TayyabAli491 <alihaniff5@gmail.com>
2025-06-05 18:56:53 +0500

    Refactor code

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,16 +1,16 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_customer!
-  before_action :set_product
+  before_action :set_product, only: %w[ create update ]
   before_action :set_cart
-  before_action :set_cart_items, only: %w[ update destroy ]
+  before_action :set_cart_item, only: %w[ update destroy ]
 
   def create
     # Either find an existing CartItem for this product, or build a new one
     cart_item = @cart.cart_items.find_or_initialize_by(product: @product)
-    cart_item.quantity = params[:quantity].to_i > 0 ? params[:quantity].to_i : 1
+    quantity = params[:quantity] || 1
 
-    return if check_stock(cart_item, @product)
+    return if check_stock_and_quantity(quantity, @product, cart_items_path)
 
     if cart_item.save
       flash[:success] = "#{@product.name} was added to your cart."
@@ -23,19 +23,15 @@ class CartItemsController < ApplicationController
 
   def update
     quantity = cart_item_params[:quantity]
-    return if check_stock(quantity, @product)
 
-    if quantity.to_i <= 0
-      flash[:alert] = "Could not update must have one item."
+    return if check_stock_and_quantity(quantity, @product, cart_path)
+
+    if @cart_item.update(cart_item_params)
+      flash[:success] = "Quantity updated."
       redirect_to cart_path
     else
-      if cart_item.update(cart_item_params)
-        flash[:success] = "Quantity updated."
-        redirect_to cart_path
-      else
-        flash[:alert] = "Could not update quantity."
-        redirect_to cart_path
-      end
+      flash[:alert] = "Could not update quantity."
+      redirect_to cart_path
     end
   end
 
@@ -67,15 +63,21 @@ class CartItemsController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
-  def set_cart_items
+  def set_cart_item
     @cart_item = @cart.cart_items.find(params[:id])
   end
 
-  def check_stock(cart_item, product)
-    if cart_item.quantity > product.stock
+  def check_stock_and_quantity(quantity, product, path)
+    if product.stock == 0
+      flash[:alert] = "Currently! #{product.name} is out of stock."
+      redirect_to path
+    elsif quantity.to_i > product.stock
       flash[:alert] = "Only #{product.stock} items available."
-      redirect_back(fallback_location: product_path(product))
+      redirect_to path
       true
+    elsif quantity.to_i <= 0
+      flash[:alert] = "Could not update must have one item."
+      redirect_to path
     end
   end
 end
