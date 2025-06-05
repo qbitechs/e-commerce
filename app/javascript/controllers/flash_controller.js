4c99de4e8390f5877170d1179a6ea55c7d133651
TayyabAli491 <alihaniff5@gmail.com>
2025-06-05 20:04:15 +0500

    Add set timeout message in flash

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["flash"]
+  connect() {
+    setTimeout(() => {
+      this.close()
+    }, 5000)
+  }
 
-  dismiss() {
-    this.flashTarget.remove()
+  close() {
+    this.element.remove()
   }
 }
