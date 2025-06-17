e5c77636f7a812b9694fac748f1a431a7c1a9d13
TayyabAli491 <alihaniff5@gmail.com>
2025-06-17 17:32:23 +0500

    Hotfix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,11 @@
 class Customer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # ToDo Customer donot delete, soft delete
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   after_create :create_cart
 
   validates :first_name, :last_name, presence: true
