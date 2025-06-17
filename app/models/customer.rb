1d08f52c21a63bacb8d9eb90545953351c02103e
TayyabAli491 <alihaniff5@gmail.com>
2025-06-17 17:03:29 +0500

    Create cart when customer is created

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,18 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :cart, dependent: :destroy
+  after_create :create_cart
+
   validates :first_name, :last_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def create_cart
+    create_cart!
   end
 end
