82da7405077821a197cd126c01976a5d36b13dc1
TayyabAli491 <alihaniff5@gmail.com>
2025-06-18 20:22:43 +0500

    Hotfix

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < Admin::ApplicationController
     @pagy, @orders = pagy(Order.includes(:customer, order_items: :product).order(created_at: :desc))
 
     @total_orders = Order.count
-    @pending_orders = Order.where(status: "Pending").count
-    @completed_orders = Order.where(status: "Completed").count
+    @pending_orders = Order.where(status: "pending").count
+    @completed_orders = Order.where(status: "completed").count
   end
 end
