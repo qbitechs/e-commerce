980774864a1bac9791c5a660e0bd1df8e797e515
TayyabAli491 <alihaniff5@gmail.com>
2025-06-18 16:39:29 +0500

    Add admin_user_signed_in? method in admin::application controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,11 @@
 class Admin::ApplicationController < ApplicationController
   include Authentication
+
+  helper_method :admin_user_signed_in?
+
+  private
+
+  def admin_user_signed_in?
+    Current.admin_user.present?
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,11 @@ class ApplicationController < ActionController::Base
 
   include Pagy::Backend
 
-  helper_method :current_cart, :admin_signed_in?, :current_admin
+  helper_method :current_cart
 
   private
 
   def current_cart
     current_customer&.cart
-  end
-
-  def current_admin
-    @current_admin ||= AdminUser.find_by(id: session[:admin_id]) if session[:admin_id]
-  end
-
-  def admin_signed_in?
-    current_admin.present?
-  end
-
-  def require_admin
-    unless admin_signed_in?
-      redirect_to admin_login_path, alert: "Please sign in to access this area."
-    end
   end
 end
