f9058b4cd2d7686100fd77f41073adee5ed0e525
TayyabAli491 <alihaniff5@gmail.com>
2025-06-19 18:24:03 +0500

    Update carts migration

--- db/migrate/20250603094706_create_carts.rb
+++ db/migrate/20250603094706_create_carts.rb
@@ -1,11 +1,12 @@
 class CreateCarts < ActiveRecord::Migration[8.0]
   def change
     create_table :carts do |t|
-      t.references :customer, null: false, index: true
+      t.references :customer, null: true, foreign_key: true
+      t.string :session_id, null: true
 
       t.timestamps
     end
 
-    add_foreign_key :carts, :customers
+    add_index :carts, :session_id
   end
 end
