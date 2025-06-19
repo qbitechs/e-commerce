cd5d9518ed072edc5aa84b31ffcb0f623842df80
TayyabAli491 <alihaniff5@gmail.com>
2025-06-19 18:36:32 +0500

    update application conntroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :set_current_cart
+
   allow_browser versions: :modern
 
   include Pagy::Backend
@@ -14,5 +15,21 @@ class ApplicationController < ActionController::Base
 
   def admin_user_signed_in?
     Current.admin_user.present?
+  end
+
+  def cart_service
+    @cart_service ||= CartService.new(session, current_customer)
+  end
+
+  def current_cart
+    cart_service.current_cart
+  end
+
+  def set_current_cart
+    @current_cart = current_cart
+  end
+
+  def merge_anonymous_cart
+    cart_service.merge_anonymous_cart_on_login(current_customer) if current_customer
   end
 end
