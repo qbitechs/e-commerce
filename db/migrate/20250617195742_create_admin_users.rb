888b2eb7a549b557d864c14c55a0ed37357f9049
TayyabAli491 <alihaniff5@gmail.com>
2025-06-27 19:13:03 +0500

    add super admin column in admin table:

--- db/migrate/20250617195742_create_admin_users.rb
+++ db/migrate/20250617195742_create_admin_users.rb
@@ -1,8 +1,9 @@
 class CreateAdminUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :admin_users do |t|
-      t.string :email_address, null: false
-      t.string :password_digest, null: false
+      t.string  :email_address, null: false
+      t.string  :password_digest, null: false
+      t.boolean :super_admin, default: false
 
       t.timestamps
     end
