17c01770bbc78d24337de058eb4349836a5e1529
TayyabAli491 <alihaniff5@gmail.com>
2025-06-27 19:40:44 +0500

    Add super admin feature

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -13,4 +13,10 @@ class Admin::ApplicationController < ApplicationController
       redirect_to root_path, alert: "You must be signed in as an admin to access that page."
     end
   end
+
+  def require_super_admin
+    unless Current.admin_user&.super_admin?
+      redirect_to admin_root_path, alert: "Access denied."
+    end
+  end
 end
