c72aeee9ef5397832166b90bf602c2e3f7b6bfbc
TayyabAli491 <alihaniff5@gmail.com>
2025-06-27 18:38:10 +0500

    Add admin authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,12 @@
 class Admin::ApplicationController < ApplicationController
   include Authentication
+  before_action :authenticate_admin!
+
+  private
+
+  def authenticate_admin!
+    unless Current.admin_user
+      redirect_to root_path, alert: "You must be signed in as an admin to access that page."
+    end
+  end
 end
