df3958909f39f441ec9bf63762a25d5feec42c56
Talha Junaid <talhajunaid65@gmail.com>
2025-07-01 00:27:16 +0500

    Update nginx.conf and added enum in custom domain model.

--- app/models/custom_domain.rb
+++ app/models/custom_domain.rb
@@ -3,4 +3,6 @@ class CustomDomain < ApplicationRecord
 
   validates :domain, presence: true, uniqueness: { scope: :store_id }
   validates_format_of :domain, with: /\A[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}\z/i, message: "must be a valid domain format"
+
+  enum :status, { active: 0, pending: 1, inactive: 2 }
 end
