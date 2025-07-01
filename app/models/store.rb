864ec33236bfa9d3a9d16c6eec947e25b69f5622
TayyabAli491 <alihaniff5@gmail.com>
2025-07-01 11:42:20 +0500

    Fix robocop issue

--- app/models/store.rb
+++ app/models/store.rb
@@ -16,5 +16,5 @@ class Store < ApplicationRecord
             presence: true,
             uniqueness: true,
             format: { with: /\A[a-z0-9]+(?:-[a-z0-9]+)*\z/ },
-            exclusion: { in: %w(www admin help support api platform) }
+            exclusion: { in: %w[www admin help support api platform] }
 end
