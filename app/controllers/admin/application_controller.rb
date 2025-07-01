ec3f09af1e6f2b3e2bc754c595e22dc1a6d10d0d
TayyabAli491 <alihaniff5@gmail.com>
2025-07-01 16:48:46 +0500

    remove custom authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,15 +4,9 @@ class Admin::ApplicationController < ApplicationController
   set_current_tenant_through_filter
 
   before_action :set_current_store
-  before_action :authenticate_admin!
+  before_action :require_authentication
 
   private
-
-  def authenticate_admin!
-    unless Current.user
-      redirect_to root_path, alert: "You must be signed in as an admin to access that page."
-    end
-  end
 
   def require_super_admin
     unless Current.user&.super_admin?
