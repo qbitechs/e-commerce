8954d852cb459e410cfe75f26e259de9be737064
TayyabAli491 <alihaniff5@gmail.com>
2025-07-01 15:40:14 +0500

    Refactor code

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,22 +9,14 @@ class Admin::ApplicationController < ApplicationController
   private
 
   def authenticate_admin!
-    unless Current.admin_user
+    unless Current.user
       redirect_to root_path, alert: "You must be signed in as an admin to access that page."
     end
   end
 
   def require_super_admin
-    unless Current.admin_user&.super_admin?
+    unless Current.user&.super_admin?
       redirect_to admin_root_path, alert: "Access denied."
-    end
-  end
-
-  private
-
-  def prevent_super_admin_direct_access
-    if Current.admin_user&.super_admin? && session[:original_admin_user_id].nil?
-      redirect_to admin_admin_users_path, alert: "Super admins must switch to as an admin to access this section."
     end
   end
 end
