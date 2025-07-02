801f900dc6973b64762a3d65917a234ce0a911e5
TayyabAli491 <alihaniff5@gmail.com>
2025-07-02 19:42:24 +0500

    Refactor admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,30 +28,17 @@ Rails.application.routes.draw do
     end
   end
 
-  # need to be refactor
   namespace :admin do
     resource :session
     resources :passwords, param: :token
 
     # Super admin routes
-    constraints lambda { |request|
-      session_id = request.cookie_jar.signed[:session_id]
-      return false unless session_id
-      session = Session.find_by(id: session_id)
-      return false unless session&.user
-      session.user.super_admin?
-    } do
+    constraints AdminTypeConstraint.new(super_admin: true) do
       root to: "admin_users#index", as: :super_admin_root
     end
 
     # Non-super admin routes
-    constraints lambda { |request|
-      session_id = request.cookie_jar.signed[:session_id]
-      return false unless session_id
-      session = Session.find_by(id: session_id)
-      return false unless session&.user
-      !session.user.super_admin?
-    } do
+    constraints AdminTypeConstraint.new(super_admin: false) do
       root to: "products#index"
       resources :products
       resources :orders, only: [ :index ]
