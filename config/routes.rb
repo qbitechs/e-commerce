f8a892ef6b038748edbc24ff29534a8a4e54d740
Talha Junaid <talhajunaid65@gmail.com>
2025-07-03 19:12:37 +0500

    Fixed host not rendering issue.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  constraints(lambda { |req| req.host != "localhost" }) do
+  constraints(lambda { |req| Rails.env.development? ? req.host != "localhost" : req.host != "e-dukaan.com" }) do
     root to: "stores#show", as: :store
 
     scope module: "store" do
