9969f729fb846f80035cbfdef7f38c6068335f66
TayyabAli491 <alihaniff5@gmail.com>
2025-07-18 15:47:29 +0500

    Add column footer_tag_line column in store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_144214) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_18_104241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -181,6 +181,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_144214) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "title"
+    t.string "footer_tag_line"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
