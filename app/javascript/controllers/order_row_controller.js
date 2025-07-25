11acaf53d8ad546fdcfe44b74239a4c728bc9cc1
TayyabAli491 <alihaniff5@gmail.com>
2025-07-25 12:37:24 +0500

    Add toggle icon

--- app/javascript/controllers/order_row_controller.js
+++ app/javascript/controllers/order_row_controller.js
@@ -2,14 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["row", "details"]
-
+  
   toggle(event) {
     const orderId = event.currentTarget.dataset.orderId
-    const detailsEl = this.detailsTargets.find(el =>
+    const detailsEl = this.detailsTargets.find(el => 
       el.dataset.orderId === orderId
     )
+    
     if (detailsEl) {
       detailsEl.classList.toggle("hidden")
+      const arrowIcon = event.currentTarget.querySelector('#arrow')
+      arrowIcon.classList.toggle('fa-chevron-down')
+      arrowIcon.classList.toggle('fa-chevron-up')
     }
   }
 }
